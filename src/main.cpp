4a73a9d28b3bf70726771c56bc55268f94f1cfdc
daeunSong <daeun7250@gmail.com>
2021-08-05 19:44:14 +0900

    Uncleaned examples for reading from tsp file

--- src/main.cpp
+++ src/main.cpp
@@ -1,104 +1,64 @@
-// ================================================================
-// Problem: TSP - Travelling salesman problem
-//
-// Date: 15/04/2016
-//
-// Authors: Rodrigo Brito (contato@rodrigobrito.net) e Bruno Gomes (bruno.nonato@ifmg.edu.br)
-//
-// ================================================================
+//This file solves a TSP using concorde given a symmetric 2D distance matrix
 
+//Info about concorde functions: www.math.uwaterloo.ca/tsp/concorde/DOC/concorde_org.html
+#include "tsp.h"
 #include <iostream>
 #include <vector>
-#include <qsopt.h>
 extern "C" {
-	#include <concorde.h>
+#include <concorde.h>
 }
 
 using namespace std;
 
-void solving_tsp_concorde(vector< vector<int> > * dist, vector<int> * rota);
+//Receive a symmetric 2D distance matrix (dist) and create a TSP optimal tour (tour)
+void solving_tsp_concorde(vector<int> * tour){
 
-int main(int argc, char* argv[]) {
-	//Dataset
-	int initialize[5][5]=
-    {
-        {0,3,4,2,7},//A
-        {3,0,4,6,3},//B
-        {4,4,0,5,8},//C
-        {2,6,5,0,6},//D
-        {7,3,8,6,0}//E
-    };
+  TSP::TSPSolver m_TSPSolver;
+  //Creating a sequential tour
+  for(int i = 0; i < tour->size(); i++)
+    tour->at(i) = i;
+  int rval = 0; //Concorde functions return 1 if something fails
+  double szeit; //Measure cpu time
 
-    //copying initial dataset for the distance structure
-    vector< vector<int> > * dist = new vector< vector<int> >( 5, vector<int>(5) );
-    for(int i = 0; i < dist->size(); i++){
-        for(int j = 0; j < dist->size(); j++){
-            dist->at(i)[j] = initialize[i][j];
-        }
-    }
+  CCrandstate rstate;
+  int seed = rand();
+  CCutil_sprand(seed, &rstate); //Initialize the portable random number generator
+  m_TSPSolver.ncount = tour->size(); //Number of nodes (cities)
 
-    //Output tour
-	vector<int> * tour = new vector<int>(5, 0);
-    //Concorde TSP proccess
-    solving_tsp_concorde(dist, tour);
+  m_TSPSolver.out_tour = CC_SAFE_MALLOC (m_TSPSolver.ncount, int);
+  m_TSPSolver.name = "/home/daeun/devel/how_to_build_concorde/src/berlin52.tsp";
+  CCdatagroup dat;
 
-    //printing final tour generated
-    cout<<"TOUR = ";
-    for(int i = 0; i < tour->size(); i++){
-        cout<<tour->at(i)<<" ";
-    }
-    cout<<endl;
-    return 0;
+  //Initialize a CCdatagroup
+  CCutil_init_datagroup (&dat);
+  CCutil_gettsplib (m_TSPSolver.name, &m_TSPSolver.ncount, &dat);
+
+  //Solves the TSP over the graph specified in the datagroup
+  rval = CCtsp_solve_dat (m_TSPSolver.ncount, &dat, m_TSPSolver.in_tour, m_TSPSolver.out_tour,
+                              m_TSPSolver.in_val, &m_TSPSolver.optval, &m_TSPSolver.success,
+                              &m_TSPSolver.foundtour, m_TSPSolver.name, m_TSPSolver.timebound,
+                              &m_TSPSolver.hit_timebound, m_TSPSolver.silent, &rstate);
+  for (int i = 0; i < m_TSPSolver.ncount; i++) {
+    tour->at(i) = m_TSPSolver.out_tour[i];
+  }
+
+  szeit = CCutil_zeit();
+  CC_IFFREE (m_TSPSolver.out_tour, int);
+//  CC_IFFREE (m_TSPSolver.name, char);
 }
 
+int main(){
 
-void solving_tsp_concorde(vector< vector<int> > * dist, vector<int> * tour) {
-    for(int i = 0; i < tour->size(); i++){
-        tour->at(i) = i;
-    }
-    if(dist->size() > 4 ){//TSP only for more than 4 elements
-        int rval = 0;
-        int semente = rand();
-        double szeit, optval, *mybnd, *mytimebound;
-        int ncount, success, foundtour, hit_timebound = 0;
-        int *in_tour = (int *) NULL;
-        int *out_tour = (int *) NULL;
-        CCrandstate rstate;
-        char *probname = (char *) NULL;
-        static int run_silently = 1;
-        CCutil_sprand(semente, &rstate);
-        mybnd = (double *) NULL;
-        mytimebound = (double *) NULL;
-        ncount = dist->size();
-        int ecount = (ncount * (ncount - 1)) / 2;
-        int *elist = new int[ecount * 2];
-        int *elen = new int[ecount];
-        int edge = 0;
-        int edge_peso = 0;
-        for (int kk = 0; kk < ncount; kk++) {
-            for (int m = kk + 1; m < ncount; m++) {
-                if (kk != m) {
-                    elist[edge] = kk;
-                    elist[edge + 1] = m;
-                    elen[edge_peso]	= dist->at(kk)[m];
-                    edge_peso++;
-                    edge = edge + 2;
-                }
-            }
-        }
-        out_tour = CC_SAFE_MALLOC (ncount, int);
-        probname = CCtsp_problabel(" ");
-        rval = CCtsp_solve_sparse(ncount, ecount, elist, elen, in_tour,
-                out_tour, mybnd, &optval, &success, &foundtour, probname,
-                mytimebound, &hit_timebound, run_silently, &rstate);
-        for (int kk = 0; kk < ncount; kk++) {
-            tour->at(kk) = out_tour[kk];
-        }
-        szeit = CCutil_zeit();
-        CC_IFFREE (elist, int);
-        CC_IFFREE (elen, int);
-        CC_IFFREE (out_tour, int);
-        CC_IFFREE (probname, char);
-    }
+  //Tour stores the optimal tour
+  vector<int> * tour = new vector<int>(52, 0);
+
+  //Solve TSP
+  solving_tsp_concorde(tour);
+
+  //Print solution
+  for(int i = 0; i < tour->size(); i++)
+    cout<<tour->at(i)+1<<"_";
+  cout << endl;
+  return 0;
 }
 
